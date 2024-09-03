9d9528fa2984288d5e86b99b32a9d97f8cc61391
ADMIN\Admin <ducanhmc24@gmail.com>
2024-09-03 17:05:42 +0700

    Duc Anh

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            <h1>
+                Chào cậu tớ là
+                <br /> Đức Anh
+            </h1>
+            <p>Tớ đang code, cậu đợi tớ nhé!</p>
+        </div>
+    );
 }
 
 export default App;
