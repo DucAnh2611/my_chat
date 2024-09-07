1ba4e30619eeb2d02c53a4ddfaf83e3e26c95c5b
ADMIN\Admin <ducanhmc24@gmail.com>
2024-09-07 23:46:07 +0700

    update

--- frontend/src/components/dialog-emoji.tsx
+++ frontend/src/components/dialog-emoji.tsx
@@ -51,7 +51,6 @@ export default function DialogSelectEmoji({
                     categories={[]}
                     lazyLoadEmojis
                     skinTonesDisabled
-                    autoFocusSearch
                 />
             </DropdownMenuContent>
         </DropdownMenu>
